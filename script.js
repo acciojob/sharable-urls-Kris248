187316284d3b4f961b4b285e2bc59a982254adb9
Kris <92295923+Kris248@users.noreply.github.com>
2025-01-11 11:38:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-// your code here
+<script> document.getElementById('button').addEventListener('click', function() { var name = document.getElementById('name').value; var year = document.getElementById('year').value; var url = 'https://localhost:8080/?name=' + encodeURIComponent(name) + '&year=' + encodeURIComponent(year); document.getElementById('url').textContent = url; }); </script>
